88c08389deefc1d4eeddbc27443f29088f289768
Lenni Marjomaa <lennima@metropolia.fi>
2022-09-08 20:31:23 +0300

    Moduulin 4 tehtävät päivitetty, t.4 valmis. Tämä tuli myöhässä, mutta ennen kuin tehtävät tarkistettiin perjantaina.

--- Moduuli/mod4t3.py
+++ Moduuli/mod4t3.py
@@ -1,6 +1,11 @@
 numero=(input ("Anna numero: "))
+lista=[]
 while numero!=(""):
     numero=(input("Anna numero: "))
-    list=numero
+    lista.append(numero)
+    if numero==(""):
+        print("Suurin annettu numeron on:"+max(lista))
+        print("Pienin annettu numero on"+min(lista))
+
 
 
